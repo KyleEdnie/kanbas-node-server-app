81ce5c65717c3e4bd58b60ec1e7938e7a489241b
Kyle Ednie <kyleednie@gmail.com>
2024-04-21 20:51:58 -0400

    initial commit

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import QuizRoutes from './Kanbas/quizzes/routes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+QuizRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000)
